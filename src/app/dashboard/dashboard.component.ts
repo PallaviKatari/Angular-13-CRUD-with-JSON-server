3a541f1fccec4a2461d8f9feed67fae93d4001a7
Miro Mac <itcodevelopment@gmail.com>
2022-02-08 11:57:44 +0200

    2. Bootstrap Modal & Form components

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArrayName, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  formValue!: FormGroup;
 
-  constructor() { }
+  constructor(private formbuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.formValue = this.formbuilder.group({
+      name:[''],
+      email:[''],
+      mobile:[''],
+      active:[''],
+    })
   }
 
 }
